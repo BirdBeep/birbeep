aed95d5b6fc29732a98c7b56500052928179a770
Ruben <ruben@gmail.com>
2018-03-19 21:37:29 +0100

    Esta puede ser la rama Master, aquí solo commit para el proyecto final

---
+++ Birbeep/src/Mensaje.java
@@ -0,0 +1,31 @@
+
+public class Mensaje {
+	private String id;
+	private Usuario emisor;
+	private Usuario receptor;
+	private String texto;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Usuario getEmisor() {
+		return emisor;
+	}
+	public void setEmisor(Usuario emisor) {
+		this.emisor = emisor;
+	}
+	public Usuario getReceptor() {
+		return receptor;
+	}
+	public void setReceptor(Usuario receptor) {
+		this.receptor = receptor;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+}
