3183643a04f4bf94255e06c191a1a8e191f3f360
Ruben <ruben@gmail.com>
2018-04-09 23:01:39 +0200

    Cambios de ubicación

---
+++ Birbeep/src/Entidades/Conversacion.java
@@ -0,0 +1,14 @@
+package Entidades;
+import java.sql.Date;
+
+public class Conversacion {
+	private Date fecha;
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+}
